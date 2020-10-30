5e0bb33d841b6e61964f9cb49a7964b12a00ef4d
Tekhamian <tekhamian@gmail.com>
2020-10-30 14:38:28 -0400

    Week 10 Assignment
    Intro to React Native

---
+++ week10/App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Button, StyleSheet, Alert, Text, SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      
+      <Text style={{color: 'black', fontSize: '20px'}}>Name: Robert Thompson</Text>
+      <Text style={{fontSize: '15px'}}>Favorite Cousines: Caribbean & Sea Foods</Text>
+      <p>
+        <Button 
+          title='Click Here To Test Button'
+          color='green' 
+          onPress={() =>alert("Thank You, Button Has Been Tested")
+          }        
+        />
+      </p>
+
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
